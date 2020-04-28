6af020b665d5ffca9412acebdae4ef62780e11f2
alexus <afidelo@protonmail.com>
2020-04-28 23:57:41 +0200

    react client added

---
+++ client/src/OtherPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-dom';
+
+export default () => {
+    return(
+        <div>
+            <text> I am some other page</text>
+            <Link to = "/"> go back home</Link> 
+        </div>
+    )
+}
