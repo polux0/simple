b679cd78a2f415afd8c25186bc1eea1401cbe005
alexus <afidelo@protonmail.com>
2020-05-03 01:46:44 +0200

    everything is set for development

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const pgClient = new Pool({
   host: keys.pgHost,
   database: keys.pgDatabase,
   password: keys.pgPassword,
-  port: keys.pgPort
+  port: keys.pgPort,
 });
 pgClient.on('error', () => console.log('Lost PG connection'));
 
--- server/keys.js
+++ server/keys.js
@@ -1,9 +1,9 @@
 module.exports = {
   redisHost: process.env.REDIS_HOST,
   redisPort: process.env.REDIS_PORT,
-  pgUser: process.env.PGUSER,
+  pgUser: process.env.POSTGRES_USER,
   pgHost: process.env.PGHOST,
-  pgDatabase: process.env.PGDATABASE,
+  pgDatabase: process.env.POSTGRES_DB,
   pgPassword: process.env.POSTGRES_PASSWORD,
   pgPort: process.env.PGPORT
 };
